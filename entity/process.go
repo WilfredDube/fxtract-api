5751a06b3f89115376db9f066adf7a587d6174c5
unbusy <unbusy@lhost.com>
2021-08-27 09:57:40 +0200

    Add process entity

---
+++ entity/process.go
@@ -0,0 +1,27 @@
+package entity
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// ProcessingPlan -
+type Process struct {
+	ID                         primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	UserID                     primitive.ObjectID   `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	CADFiles                   []primitive.ObjectID `json:"cadfiles" bson:"cadfiles,omitempty" validate:"empty=false"`
+	ProcessType                ProcessType          `json:"process_type" bson:"process_type" validate:"empty=false"`
+	Status                     Status               `json:"status" bson:"status" validate:"empty=false"`
+	Quantity                   int64                `json:"quantity" bson:"quantity" validate:"empty=false"`
+	ProcessingTime             float64              `json:"processing_time" bson:"processing_time" validate:"empty=false"`
+	EstimatedManufacturingTime float64              `json:"estimated_manufacturing_time" bson:"estimated_manufacturing_time" validate:"empty=false"`
+	TotalCost                  float64              `json:"total_cost" bson:"total_cost" validate:"empty=false"`
+	CreatedAt                  int64                `json:"created_at" bson:"created_at" validate:"empty=false"`
+}
+
+type ProcessType string
+type Status string
+
+const (
+	FRE         ProcessType = "Feature recognition"
+	PP          ProcessType = "Process planning"
+	Complete    Status      = "Complete"
+	Pprocessing Status      = "Processing"
+)
