c1309639eb63fbf96c895488ef925eb4b3cf37b7
unbusy <unbusy@lhost.com>
2021-09-03 21:42:40 +0200

    change CADFiles data type to []string

--- entity/task.go
+++ entity/task.go
@@ -4,16 +4,16 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // ProcessingPlan -
 type Task struct {
-	TaskID                     primitive.ObjectID   `json:"task_id,omitempty" bson:"task_id,omitempty"`
-	UserID                     primitive.ObjectID   `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	CADFiles                   []primitive.ObjectID `json:"cadfiles" bson:"cadfiles,omitempty" validate:"empty=false"`
-	ProcessType                []ProcessType        `json:"process_type" bson:"process_type" validate:"empty=false"`
-	Status                     Status               `json:"status" bson:"status" validate:"empty=false"`
-	Quantity                   int64                `json:"quantity" bson:"quantity" validate:"empty=false"`
-	ProcessingTime             float64              `json:"processing_time" bson:"processing_time" validate:"empty=false"`
-	EstimatedManufacturingTime float64              `json:"estimated_manufacturing_time" bson:"estimated_manufacturing_time" validate:"empty=false"`
-	TotalCost                  float64              `json:"total_cost" bson:"total_cost" validate:"empty=false"`
-	CreatedAt                  int64                `json:"created_at" bson:"created_at" validate:"empty=false"`
+	TaskID                     primitive.ObjectID `json:"task_id,omitempty" bson:"task_id,omitempty"`
+	UserID                     primitive.ObjectID `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	CADFiles                   []string           `json:"cadfiles" bson:"cadfiles,omitempty" validate:"empty=false"`
+	ProcessType                []ProcessType      `json:"process_type" bson:"process_type" validate:"empty=false"`
+	Status                     Status             `json:"status" bson:"status" validate:"empty=false"`
+	Quantity                   int64              `json:"quantity" bson:"quantity" validate:"empty=false"`
+	ProcessingTime             float64            `json:"processing_time" bson:"processing_time" validate:"empty=false"`
+	EstimatedManufacturingTime float64            `json:"estimated_manufacturing_time" bson:"estimated_manufacturing_time" validate:"empty=false"`
+	TotalCost                  float64            `json:"total_cost" bson:"total_cost" validate:"empty=false"`
+	CreatedAt                  int64              `json:"created_at" bson:"created_at" validate:"empty=false"`
 }
 
 type ProcessType string
