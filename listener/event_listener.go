757dc1987cc2dbafb76be9d8479fa244f9744eab
unbusy <unbusy@lhost.com>
2021-09-09 22:03:45 +0200

    Add processing plan complete listener

--- listener/event_listener.go
+++ listener/event_listener.go
@@ -3,17 +3,20 @@ package listener
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/WilfredDube/fxtract-backend/entity"
 	"github.com/WilfredDube/fxtract-backend/lib/contracts"
 	"github.com/WilfredDube/fxtract-backend/lib/msgqueue"
 	"github.com/WilfredDube/fxtract-backend/service"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type EventProcessor struct {
-	EventListener  msgqueue.EventListener
-	CadFileService service.CadFileService
-	TaskService    service.TaskService
+	EventListener         msgqueue.EventListener
+	CadFileService        service.CadFileService
+	TaskService           service.TaskService
+	ProcessingPlanService service.ProcessingPlanService
 }
 
 func (p *EventProcessor) ProcessEvents() {
@@ -69,6 +72,57 @@ func (p *EventProcessor) handleEvent(event msgqueue.Event) {
 		}
 
 		log.Printf("[ User: %s > TaskID: %s > Task status: %s]: CAD file (%s) features saved successfully!", e.UserID, returedTask.TaskID, returedTask.Status, e.CADFileID)
+		log.Printf("==========================================================")
+	case *contracts.ProcessPlanningComplete:
+		log.Printf("event %s created: %s", e.CADFileID, e.TaskID)
+		log.Printf("==========================================================")
+		fmt.Printf("Received a Processing plan for CAD file ID: %v\n", e.ProcessingPlan.CADFileID)
+
+		processingPlan := entity.ProcessingPlan{}
+		processingPlan.ID = primitive.NewObjectID()
+		processingPlan.CADFileID = e.ProcessingPlan.CADFileID
+
+		cadFile, err := p.CadFileService.Find(processingPlan.CADFileID.Hex())
+		if err != nil {
+			log.Fatalf("%s: %s", "Cadfile does not exist ", err)
+		}
+
+		processingPlan.Rotations = e.ProcessingPlan.Rotations
+		processingPlan.Flips = e.ProcessingPlan.Flips
+		processingPlan.Tools = e.ProcessingPlan.Tools
+		processingPlan.Modules = e.ProcessingPlan.Modules
+		processingPlan.ProcessingTime = e.ProcessingPlan.ProcessingTime
+		processingPlan.EstimatedManufacturingTime = e.ProcessingPlan.EstimatedManufacturingTime
+		processingPlan.TotalToolDistance = e.ProcessingPlan.TotalToolDistance
+		processingPlan.BendingSequences = e.ProcessingPlan.BendingSequences
+		processingPlan.Quantity = e.ProcessingPlan.Quantity
+		processingPlan.CreatedAt = time.Now().Unix()
+
+		_, err = p.ProcessingPlanService.Create(&processingPlan)
+		if err != nil {
+			log.Fatalf("%s: %s", "Failed to save processing plan: ", err)
+		}
+
+		cadFile.FeatureProps.ProcessLevel = e.ProcessLevel
+		_, err = p.CadFileService.Update(*cadFile)
+		if err != nil {
+			log.Fatalf("%s: %s", "Cadfile update failed ", err)
+		}
+
+		task, err := p.TaskService.Find(e.TaskID)
+		if err != nil {
+			log.Fatalf("%s: %s", "Failed to retrieve task data: ", err)
+		}
+
+		task.Status = entity.Complete
+		task.ProcessingTime = e.ProcessingPlan.EstimatedManufacturingTime
+
+		returedTask, err := p.TaskService.Update(*task)
+		if err != nil {
+			log.Fatalf("%s: %s", "Failed to update data: ", err)
+		}
+
+		log.Printf("[ User: %s > TaskID: %s > Task status: %s]: CAD file (%s) processing plan saved successfully!", e.UserID, returedTask.TaskID, returedTask.Status, e.CADFileID)
 		log.Printf("==========================================================")
 	default:
 		log.Printf("unknown event type: %T", e)
