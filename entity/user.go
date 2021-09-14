d7addbbd8a3826de3fef08e7f3bcb9a5733f8e9b
unbusy <unbusy@lhost.com>
2021-09-14 12:18:30 +0200

    Add new User attributes

--- entity/user.go
+++ entity/user.go
@@ -1,6 +1,8 @@
 package entity
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Role int
 
@@ -11,11 +13,13 @@ const (
 
 // User -
 type User struct {
-	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Firstname string             `json:"firstname" bson:"firstname" validate:"empty=false"`
-	Lastname  string             `json:"lastname" bson:"lastname" validate:"empty=false"`
-	Email     string             `json:"email" bson:"email" validate:"empty=false & format=email"`
-	Password  string             `json:"password" bson:"password" validate:"empty=false"`
-	UserRole  Role               `json:"role" bson:"role" validate:"empty=false"`
-	CreatedAt int64              `json:"created_at" bson:"created_at" validate:"empty=false"`
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Firstname  string             `json:"firstname" bson:"firstname" validate:"empty=false"`
+	Lastname   string             `json:"lastname" bson:"lastname" validate:"empty=false"`
+	Email      string             `json:"email" bson:"email" validate:"empty=false & format=email"`
+	Password   string             `json:"password" bson:"password" validate:"empty=false"`
+	UserRole   Role               `json:"role" bson:"role" validate:"empty=false"`
+	IsVerified bool               `json:"isverified" bson:"isverified" validate:"empty=false"`
+	CreatedAt  int64              `json:"created_at" bson:"created_at" validate:"empty=false"`
+	UpdatedAt  int64              `json:"updated_at" bson:"updated_at"`
 }
